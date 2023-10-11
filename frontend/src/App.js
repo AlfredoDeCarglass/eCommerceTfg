4dff49f6a9fe65844b98a742ecf66d4894058bcb
AlfredoDeCarglass <alfredo.cft@gmail.com>
2023-10-11 14:07:36 +0200

    create React App

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <a href="/">eCommerce</a>
+      </header>
+      <main>Lista de productos</main>
+    </div>
+  );
+}
+
+export default App;
