3d482fc0d941e00c63d9f43170565d012f43e2a5
AlfredoDeCarglass <alfredo.cft@gmail.com>
2023-10-11 18:53:28 +0200

    create Node Server

---
+++ backend/data.js
@@ -0,0 +1,70 @@
+const data = {
+  products: [
+    {
+      name: 'Vino Tinto Reserva La Mancha',
+      slug: 'vino-tinto-reserva-lamancha',
+      category: 'Vinos',
+      image: '/images/v1.png',
+      price: 3.9,
+      countInStock: 20,
+      brand: 'Bodegas Corral',
+      rating: 4.5,
+      numReviews: 10,
+      descripcion:
+        'Botella de 75 cl. Vino ecológico: Vino elaborado con uvas ecológicas seleccionadas en nuestros viñedos.',
+    },
+    {
+      name: 'Vino Espumoso D.O. La Mancha',
+      slug: 'vino-espumoso',
+      category: 'Vinos',
+      image: '/images/v2.png',
+      price: 30,
+      countInStock: 20,
+      brand: 'Bodegas Francia',
+      rating: 4.5,
+      numReviews: 10,
+      descripcion:
+        'Botella de 75 cl. Vino ecológico: Vino elaborado con uvas ecológicas seleccionadas en nuestros viñedos.',
+    },
+    {
+      name: 'Vino Blanco Airen',
+      slug: 'vino-blanco-airen',
+      category: 'Vinos',
+      image: '/images/v3.png',
+      price: 30,
+      countInStock: 20,
+      brand: 'Bodegas Corral',
+      rating: 4.5,
+      numReviews: 10,
+      descripcion:
+        'Botella de 75 cl. Vino ecológico: Vino elaborado con uvas ecológicas seleccionadas en nuestros viñedos.',
+    },
+    {
+      name: 'Vino Blanco Chardonnay',
+      slug: 'vino-blanco-chardonnay',
+      category: 'Vinos',
+      image: '/images/v4.png',
+      price: 30,
+      countInStock: 20,
+      brand: 'Bodegas Almaguer',
+      rating: 4.5,
+      numReviews: 10,
+      descripcion:
+        'Botella de 75 cl. Vino ecológico: Vino elaborado con uvas ecológicas seleccionadas en nuestros viñedos.',
+    },
+    {
+      name: 'Vino Frizzante',
+      slug: 'vino-frizzante',
+      category: 'Vinos',
+      image: '/images/v5.png',
+      price: 30,
+      countInStock: 20,
+      brand: 'Bodegas Almaguer',
+      rating: 4.5,
+      numReviews: 10,
+      descripcion:
+        'Botella de 75 cl. Vino ecológico: Vino elaborado con uvas ecológicas seleccionadas en nuestros viñedos.',
+    },
+  ],
+};
+export default data;
