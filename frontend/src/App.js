983457795465df4a2ba2ba6a9ef8ca532168b139
AlfredoDeCarglass <alfredo.cft@gmail.com>
2023-10-11 16:28:37 +0200

    List Products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,32 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header className="App-header">
         <a href="/">eCommerce</a>
       </header>
-      <main>Lista de productos</main>
+      <main>
+        <h1>Productos Destacados</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  {product.price} <strong>€</strong>
+                </p>
+                <button>Añadir al carro</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
---
+++ frontend/src/data.js
@@ -0,0 +1,57 @@
+const data = {
+  products: [
+    {
+      name: 'Queso Curado Manchego',
+      slug: 'queso-curado-manchego',
+      category: 'Quesos',
+      image: '/images/queso1.png',
+      price: 30,
+      countInStock: 20,
+      brand: 'Quesos Familia G',
+      rating: 4.5,
+      numReviews: 10,
+      descripcion:
+        'Los quesos curados son quesos que se han sometido a un proceso de curación o a un añejado. Un proceso por el cual se secan y se aplican técnicas de conservación como el ahumado o el salado.',
+    },
+    {
+      name: 'Vino Blanco La Mancha',
+      slug: 'vino-blanco-lamancha',
+      category: 'Vinos',
+      image: '/images/vino1.png',
+      price: 30,
+      countInStock: 20,
+      brand: 'Bodegas N',
+      rating: 4.5,
+      numReviews: 10,
+      descripcion:
+        'Vino ecológico: Vino elaborado con uvas ecológicas seleccionadas en nuestros viñedos.',
+    },
+    {
+      name: 'Aceite de Oliva Montes de Toledo',
+      slug: 'aceite-oliva',
+      category: 'Aceites',
+      image: '/images/aceite1.png',
+      price: 30,
+      countInStock: 20,
+      brand: 'Almazara L',
+      rating: 4.5,
+      numReviews: 10,
+      descripcion:
+        'Es un Aceite de Oliva Virgen Extra cuyo origen demuestra maestría y tradición. Denominación de Origen Montes de Toledo.',
+    },
+    {
+      name: 'Vino frizzante',
+      slug: 'vino-frizzante',
+      category: 'Vinos',
+      image: '/images/vino2.png',
+      price: 30,
+      countInStock: 20,
+      brand: 'Bodegas n',
+      rating: 4.5,
+      numReviews: 10,
+      descripcion:
+        'Los vinos frizzantes son aquellos que ofrecen cierta burbuja sin llegar a estar considerados propiamente como vinos espumosos.',
+    },
+  ],
+};
+export default data;
